45f8d259d642e79a653f329fc8a483dc8cb64e74
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2017-12-06 18:02:49 +0100

    Refactor

--- Project1/source.cpp
+++ Project1/source.cpp
@@ -6,29 +6,28 @@
 using namespace std;
 using namespace cv;
 
-/* Uchwyt funkcji */
-void myMethode(Mat frame);
+void DetectCircles(Mat frame);
+void ChangeParameters(int c);
+void PrintResultAndParameters(Mat frame_amount_color);
+void DrawCircles(Mat frame, vector<Vec3f> circlesToDraw, String windowName);
 
-/* Zmienne globalne */
-String window_name = "Detekcja oczek na kostce do gry!";
-
-vector<int> buffor;
 ArrayOfDrawablePoints arrayOfPoints;
 
-int minDist = 8; // NIE ZMNIEJSZAÆ
+/// Parameters for Houghs transform
+int minDist = 8;
 int upperThreshold = 200;
-int centersThreshold = 14; // NIE ZMNIEJSZAÆ
+int centersThreshold = 14;
 int minRadius = 1;
 int maxRadius = 10;
 
-/* Main */
 int main(void)
 {
 	VideoCapture capture;
 	Mat frame;
 	Mat frame_amount_color;
 
-	//Czytaj wideo stream - domyœlna kamera
+	String windowName = "Detekcja oczek na kostce do gry!";
+
 	capture.open(0);
 	if (!capture.isOpened()) 
 	{ 
@@ -41,94 +40,104 @@ int main(void)
 		if (frame.empty())
 		{
 			printf("Brak przechwytywanego obrazu, break!");
-			break;
+			return -2;
 		}
 
-		//Wywolanie uchwytu 
-		//detectAndDisplay(detector, frame);
+		int c = waitKey(100);
+		
+		if ((char)c == 27) 
+			return 0; 
+
+		ChangeParameters(c);
+
 		cvtColor(frame, frame_amount_color, COLOR_BGR2HLS_FULL);
 		
-		int c = waitKey(100);
-		if ((char)c == 27) { break; } // escape
+		DetectCircles(frame);
 
-		if ((char)c == '1')
-			minDist++;
-		if ((char)c == 'q')
-			minDist--;
+		DrawCircles(frame, arrayOfPoints.GetDrawablePoints(), windowName);
 
-		if ((char)c == '2')
-			upperThreshold++;
-		if ((char)c == 'w')
-			upperThreshold--;
+		PrintResultAndParameters(frame_amount_color);
 
-		if ((char)c == '3')
-			centersThreshold++;
-		if ((char)c == 'e')
-			centersThreshold--;
-
-		if ((char)c == '4')
-			minRadius++;
-		if ((char)c == 'r')
-			minRadius--;
-
-		if ((char)c == '5')
-			maxRadius++;
-		if ((char)c == 't')
-			maxRadius--;
-
-		myMethode(frame);
-
-		
-		system("cls");
-		cout << "H - odcien; L - jasnoœæ, S - nasycenie" << endl;
-		cout << "Light: " << mean(frame_amount_color) << endl;
-		cout << "Iloœæ oczek: " << arrayOfPoints.GetDrawablePoints().size() << endl;
-		cout << "Minimalny dystans: " << minDist << endl;
-		cout << "Próg dla Canny: " << upperThreshold << endl;
-		cout << "Próg dla centrów: " << centersThreshold << endl;
-		cout << "Minimalny promieñ: " << minRadius << endl;
-		cout << "Maksymalny promieñ: " << maxRadius << endl;
-		
+		frame_amount_color.release();
+		frame.release();
 	}
 	return 0;
 }
 
-void myMethode(Mat frame)
+void DetectCircles(Mat frame)
 {
 	Mat frame_gray;
-	/// Convert it to gray
+	
 	cvtColor(frame, frame_gray, CV_BGR2GRAY);
 
 	Mat blurredGray;
-	/// Reduce the noise so we avoid false circle detection
+	
 	GaussianBlur(frame_gray, blurredGray, Size(9, 9), 2, 2);
-
-	//imshow(window_name, frame_gray);
 
 	vector<Vec3f> circles;
 
-	/// Apply the Hough Transform to find the circles
 	HoughCircles(blurredGray, circles, CV_HOUGH_GRADIENT, 1, minDist, upperThreshold, centersThreshold, minRadius, maxRadius);
 
 	arrayOfPoints.RefreshArray(circles);
-	vector<Vec3f> pointsToDraw = arrayOfPoints.GetDrawablePoints();
-	for (int i = 0; i < pointsToDraw.size(); i++)
-	{
-		Point center(cvRound(pointsToDraw[i][0]), cvRound(pointsToDraw[i][1]));
-		int radius = cvRound(pointsToDraw[i][2]);
-		// circle center
-		circle(frame, center, 3, Scalar(0, 255, 0), -1, 8, 0);
-		// circle outline
-		circle(frame, center, radius, Scalar(0, 0, 255), 3, 8, 0);
-	}
-
-
-	cout << "Chwilowa iloœæ obiektów: " << circles.size() << endl;
 
 	circles.clear();
-	pointsToDraw.clear();
-
-	imshow(window_name, frame);
 	frame.release();
 	frame_gray.release();
+	blurredGray.release();
+}
+
+void DrawCircles(Mat frame, vector<Vec3f> circlesToDraw, String windowName)
+{
+	for (int i = 0; i < circlesToDraw.size(); i++)
+	{
+		Point center(cvRound(circlesToDraw[i][0]), cvRound(circlesToDraw[i][1]));
+		int radius = cvRound(circlesToDraw[i][2]);
+		circle(frame, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+		circle(frame, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+	}
+	imshow(windowName, frame);
+	frame.release();
+	circlesToDraw.clear();
+}
+
+void ChangeParameters(int c)
+{
+	if ((char)c == '1')
+		minDist++;
+	if ((char)c == 'q')
+		minDist--;
+
+	if ((char)c == '2')
+		upperThreshold++;
+	if ((char)c == 'w')
+		upperThreshold--;
+
+	if ((char)c == '3')
+		centersThreshold++;
+	if ((char)c == 'e')
+		centersThreshold--;
+
+	if ((char)c == '4')
+		minRadius++;
+	if ((char)c == 'r')
+		minRadius--;
+
+	if ((char)c == '5')
+		maxRadius++;
+	if ((char)c == 't')
+		maxRadius--;
+}
+
+void PrintResultAndParameters(Mat frame_amount_color)
+{
+	system("cls");
+	cout << "H - odcien; L - jasnoœæ, S - nasycenie" << endl;
+	cout << "Light: " << mean(frame_amount_color) << endl;
+	cout << "Iloœæ oczek: " << arrayOfPoints.GetDrawablePoints().size() << endl;
+	cout << "Minimalny dystans: " << minDist << endl;
+	cout << "Próg dla Canny: " << upperThreshold << endl;
+	cout << "Próg dla centrów: " << centersThreshold << endl;
+	cout << "Minimalny promieñ: " << minRadius << endl;
+	cout << "Maksymalny promieñ: " << maxRadius << endl;
+	frame_amount_color.release();
 }
